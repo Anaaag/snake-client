a88f020eba93b660916ff2fa0ed32e40c5c4ac38
Anaaag <agarc20@outlook.com>
2022-04-13 14:40:38 -0700

    update connect function

---
+++ play.js
@@ -0,0 +1,29 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function (data) {
+  console.log("Connecting ...");
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  conn.on("connect", () => {
+    console.log("Connected to server")
+  })
+
+  conn.on("data", (data) => {
+    console.log("Server says;", data)
+  })
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+
+
+  return conn;
+};
+
+connect();
+
