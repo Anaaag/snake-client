2a0c3d08b6b57bb82e933d25694659477a125d8c
Anaaag <agarc20@outlook.com>
2022-04-14 00:01:06 -0700

    update play.js

--- play.js
+++ play.js
@@ -1,4 +1,7 @@
-const connect = require("./client")
-const setupInput = require("./input")
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
+const conn = connect()
+
 
 
